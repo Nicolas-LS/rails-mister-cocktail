3666cf4caac5025887516298330108897099c8c4
Nicolas Lutz Sorg <mini_lutz@msn.com>
2020-11-12 17:02:34 +0000

    Mister Cocktail

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,60 +1,36 @@
 class DosesController < ApplicationController
-  before_action :set_dose, only: [:show, :edit, :update, :destroy]
+  before_action :set_cocktail, except: :destroy
 
-  # GET /doses
-  def index
-    @doses = Dose.all
-  end
-
-  # GET /doses/1
-  def show
-    @ingredient = Dose.new
-  end
-
-  # GET /doses/new
   def new
     @dose = Dose.new
   end
 
-  # GET /doses/1/edit
-  def edit
-  end
-
-  # POST /doses
   def create
     @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
 
     if @dose.save
-      redirect_to @dose, notice: 'dose was successfully created.'
+      redirect_to cocktail_path(@cocktail), notice: 'dose was successfully created.'
     else
       render :new
     end
   end
 
-  # PATCH/PUT /doses/1
-  def update
-    if @dose.update(dose_params)
-      redirect_to @dose, notice: 'dose was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /doses/1
   def destroy
+    @dose = Dose.find(params[:id])
+    cocktail = @dose.cocktail
     @dose.destroy
-    redirect_to doses_url, notice: 'dose was successfully destroyed.'
+    redirect_to cocktail_path(cocktail), notice: 'dose was successfully destroyed.'
   end
 
   private
 
   # Use callbacks to share common setup or constraints between actions.
-  def set_dose
-    @dose = Dose.find(params[:id])
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
-  # Only allow a list of trusted parameters through.
   def dose_params
-    params.require(:dose).permit(:name)
+    params.require(:dose).permit(:ingredient_id, :description)
   end
 end
